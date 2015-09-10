cc81d70b73bfd958d966a5f626356651bf890a44
Alex <acidy84@users.noreply.github.com>
2015-09-11 02:26:23 +0300

    Update Vkontakte.php

--- src/Vkontakte.php
+++ src/Vkontakte.php
@@ -5,9 +5,8 @@ namespace J4k\OAuth2\Client\Provider;
 use League\OAuth2\Client\Entity\User as OAuthUser;
 use League\OAuth2\Client\Token\AccessToken;
 use League\OAuth2\Client\Provider\AbstractProvider;
-use League\OAuth2\Client\Provider\Exception\VkontakteProviderException;
+use J4k\OAuth2\Client\Provider\Exception\VkontakteProviderException;
 use League\OAuth2\Client\Provider\Exception\IdentityProviderException;
-use League\OAuth2\Client\Provider\VkontakteResourceOwner;
 use Psr\Http\Message\ResponseInterface;
 
 
@@ -29,6 +28,8 @@ class Vkontakte extends AbstractProvider
     public function getResourceOwnerDetailsUrl(AccessToken $token)
     {
         $fields = [
+            'first_name',
+            'last_name',
             'nickname',
             'screen_name',
             'sex',
@@ -53,9 +54,8 @@ class Vkontakte extends AbstractProvider
             'can_post',
             'universities',
             'schools',
-            'verified', ];
+            'verified'];
 
-        //dd($token);
         $userId = $token->getResourceOwnerId();
         $tokenValue = $token->getToken();
         return "https://api.vk.com/method/users.get?user_id={$userId}&fields="
@@ -69,128 +69,8 @@ class Vkontakte extends AbstractProvider
     
     public function getAccessToken($grant = 'authorization_code', array $params = [])
     {
-        /*if (is_string($grant)) {
-            // PascalCase the grant. E.g: 'authorization_code' becomes 'AuthorizationCode'
-            $className = str_replace(' ', '', ucwords(str_replace(['-', '_'], ' ', $grant)));
-            $grant = 'League\\OAuth2\\Client\\Grant\\'.$className;
-            if (! class_exists($grant)) {
-                throw new \InvalidArgumentException('Unknown grant "'.$grant.'"');
-            }
-            $grant = new $grant();
-        } elseif (! $grant instanceof GrantInterface) {
-            $message = get_class($grant).' is not an instance of League\OAuth2\Client\Grant\GrantInterface';
-            throw new \InvalidArgumentException($message);
-        }
-
-        $defaultParams = [
-            'client_id'     => $this->clientId,
-            'client_secret' => $this->clientSecret,
-            'redirect_uri'  => $this->redirectUri,
-            'grant_type'    => $grant,
-        ];
-
-        $requestParams = $grant->prepRequestParams($defaultParams, $params);
-
-        try {
-            switch (strtoupper($this->method)) {
-                case 'GET':
-                    // @codeCoverageIgnoreStart
-                    // No providers included with this library use get but 3rd parties may
-                    $client = $this->getHttpClient();
-                    $client->setBaseUrl($this->getBaseAccessTokenUrl() . '?' . $this->httpBuildQuery($requestParams, '', '&'));
-                    $request = $client->get(null, null, $requestParams)->send();
-                    $response = $request->getBody();
-                    break;
-                    // @codeCoverageIgnoreEnd
-                case 'POST':
-                    $client = $this->getHttpClient();
-                    $client->setBaseUrl($this->getBaseAccessTokenUrl());
-                    $request = $client->post(null, null, $requestParams)->send();
-                    $response = $request->getBody();
-                    break;
-                // @codeCoverageIgnoreStart
-                default:
-                    throw new \InvalidArgumentException('Neither GET nor POST is specified for request');
-                // @codeCoverageIgnoreEnd
-            }
-        } catch (BadResponseException $e) {
-            // @codeCoverageIgnoreStart
-            $response = $e->getResponse()->getBody();
-            // @codeCoverageIgnoreEnd
-        }
-
-        switch ($this->responseType) {
-            case 'json':
-                $result = json_decode($response, true);
-
-                if (JSON_ERROR_NONE !== json_last_error()) {
-                    $result = [];
-                }
-
-                break;
-            case 'string':
-                parse_str($response, $result);
-                break;
-        }
-
-        if (isset($result['error']) && ! empty($result['error'])) {
-            // @codeCoverageIgnoreStart
-            throw new IDPException($result);
-            // @codeCoverageIgnoreEnd
-        }
-
-        $result = $this->prepareAccessTokenResult($result);
-
-        $accessToken = $grant->handleResponse($result);
-        */
-        var_dump('OK');
         $accessToken = parent::getAccessToken($grant, $params);
-
-        //dd($accessToken);
-        // Add email from response
-        if (!empty($result['email'])) {
-            $accessToken->email = $result['email'];
-        }
         return $accessToken;
-    }
-
-    public function userDetails($response, AccessToken $token)
-    {
-        $response = $response->response[0];
-        $user = new OAuthUser();
-        $email = (isset($token->email)) ? $token->email : null;
-        $location = (isset($response->country)) ? $response->country : null;
-        $description = (isset($response->status)) ? $response->status : null;
-
-        $user->exchangeArray([
-            'uid' => $response->uid,
-            'nickname' => $response->nickname,
-            'name' => $response->screen_name,
-            'firstname' => $response->first_name,
-            'lastname' => $response->last_name,
-            'email' => $email,
-            'location' => $location,
-            'description' => $description,
-            'imageUrl' => $response->photo_200_orig,
-        ]);
-        return $user;
-    }
-
-    public function userUid($response, AccessToken $token)
-    {
-        $response = $response->response[0];
-        return $response->uid;
-    }
-
-    public function userEmail($response, AccessToken $token)
-    {
-        return (isset($token->email)) ? $token->email : null;
-    }
-
-    public function userScreenName($response, AccessToken $token)
-    {
-        $response = $response->response[0];
-        return [$response->first_name, $response->last_name];
     }
 
     protected function checkResponse(ResponseInterface $response, $data)
@@ -206,8 +86,7 @@ class Vkontakte extends AbstractProvider
 
     protected function createResourceOwner(array $response, AccessToken $token)
     {
-        //var_dump($response);
-        return new VkontakteResourceOwner($response);
+        return new VkontakteResourceOwner($response, $token);
     }
 
     protected function getAccessTokenResourceOwnerId()
@@ -215,9 +94,12 @@ class Vkontakte extends AbstractProvider
         return 'user_id';
     }
 
-    protected function prepareAccessTokenResponse(array $result)
+    protected function createAccessToken(array $response, \League\OAuth2\Client\Grant\AbstractGrant $grant)
     {
-        //dd(parent::prepareAccessTokenResponse($result));
-        return parent::prepareAccessTokenResponse($result);
+        $token = parent::createAccessToken($response, $grant);
+        if (!empty($response['email'])) {
+            $token->email = $response['email'];
+        }
+        return $token;
     }
-} 
+}
